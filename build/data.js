f470b8624d6fd2206fa3d0c36023e4d9ecf7d7d2
RGBboy <l-_-l@rgbboy.com>
2022-06-24 17:52:37 +0100

    update product data

--- build/data.js
+++ build/data.js
@@ -11,7 +11,7 @@ const data = {
             "id": "gid://shopify/Product/7295549046979",
             "handle": "bubble-light-sculpture-edition-4b04",
             "title": "Bubble Light Sculpture - Edition 4B04",
-            "descriptionHtml": "<p>Dimensions: ? x ? x ?</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>A beautiful light sculpture created using a handcrafted bubble algorithm. Each edition of this sculpture is its own unique permutation of the algorithm. The base is handmade ceramic and encases an energy efficient, USB powered LED light. The shade is 3D printed in recycled plastic and when pressed gently from above, turns the light on and off with a satisfying click.</p>\n<p><b>Materials:</b> Recycled plastic, ceramic, LED light</p>\n<p><b>Light source:</b> Built-in 1w LED light. Fits a standard USB-A socket (wall adapter not included)</p>\n<p><b>Light colour:</b> Warm white (2700 Kelvin)</p>\n<p><b>Dimensions:</b> 64mm x 64mm x 130mm, cable length 2m</p>\n<p>Made in London, UK.</p>\n<p>We care about our impact on the planet, so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": true,
             "priceRange": {
               "maxVariantPrice": {
@@ -21,17 +21,17 @@ const data = {
             },
             "featuredImage": {
               "altText": null,
-              "url": "https://cdn.shopify.com/s/files/1/0617/8750/4835/products/4B04-on_095d676e-4eaf-4ff6-ab59-900034e67216_512x512.jpg?v=1655657024"
+              "url": "https://cdn.shopify.com/s/files/1/0617/8750/4835/products/4B04-off_fa420777-7cca-44f8-a3f4-d235fe56e81b_512x512.jpg?v=1656083641"
             },
             "images": {
               "nodes": [
                 {
                   "altText": null,
-                  "url": "https://cdn.shopify.com/s/files/1/0617/8750/4835/products/4B04-on_095d676e-4eaf-4ff6-ab59-900034e67216_512x512.jpg?v=1655657024"
+                  "url": "https://cdn.shopify.com/s/files/1/0617/8750/4835/products/4B04-off_fa420777-7cca-44f8-a3f4-d235fe56e81b_512x512.jpg?v=1656083641"
                 },
                 {
                   "altText": null,
-                  "url": "https://cdn.shopify.com/s/files/1/0617/8750/4835/products/4B04-off_fa420777-7cca-44f8-a3f4-d235fe56e81b_512x512.jpg?v=1655657024"
+                  "url": "https://cdn.shopify.com/s/files/1/0617/8750/4835/products/4B04-on_095d676e-4eaf-4ff6-ab59-900034e67216_512x512.jpg?v=1656083641"
                 }
               ]
             }
@@ -40,7 +40,7 @@ const data = {
             "id": "gid://shopify/Product/7298460090563",
             "handle": "bubble-light-sculpture-edition-4b28",
             "title": "Bubble Light Sculpture - Edition 4B28",
-            "descriptionHtml": "<p>Dimensions: ? x ? x ?</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>A beautiful light sculpture created using a handcrafted bubble algorithm. Each edition of this sculpture is its own unique permutation of the algorithm. The base is handmade ceramic and encases an energy efficient, USB powered LED light. The shade is 3D printed in recycled plastic and when pressed gently from above, turns the light on and off with a satisfying click.</p>\n<p><b>Materials:</b> Recycled plastic, ceramic, LED light</p>\n<p><b>Light source:</b> Built-in 1w LED light. Fits a standard USB-A socket (wall adapter not included)</p>\n<p><b>Light colour:</b> Warm white (2700 Kelvin)</p>\n<p><b>Dimensions:</b> 68mm x 68mm x 130mm, cable length 2m</p>\n<p>Made in London, UK.</p>\n<p>We care about our impact on the planet, so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": true,
             "priceRange": {
               "maxVariantPrice": {
@@ -69,7 +69,7 @@ const data = {
             "id": "gid://shopify/Product/7295555272899",
             "handle": "popcorn-light-sculpture-edition-5c04",
             "title": "Popcorn Light Sculpture - Edition 5C04",
-            "descriptionHtml": "<p>Dimensions: ? x ? x ?</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>A beautiful light sculpture created using a handcrafted algorithm. Each edition of this sculpture is its own unique permutation of the algorithm. The base is handmade ceramic and encases an energy efficient, USB powered LED light. The shade is 3D printed in recycled plastic and when pressed gently from above, turns the light on and off with a satisfying click.</p>\n<p><b>Materials:</b> Recycled plastic, ceramic, LED light</p>\n<p><b>Light source:</b> Built-in 1w LED light. Fits a standard USB-A socket (wall adapter not included)</p>\n<p><b>Light colour:</b> Warm white (2700 Kelvin)</p>\n<p><b>Dimensions:</b> 50mm x 50mm x 125mm, cable length 2m</p>\n<p>Made in London, UK.</p>\n<p>We care about our impact on the planet, so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": true,
             "priceRange": {
               "maxVariantPrice": {
@@ -98,7 +98,7 @@ const data = {
             "id": "gid://shopify/Product/7295554519235",
             "handle": "popcorn-light-sculpture-edition-5c1d",
             "title": "Popcorn Light Sculpture - Edition 5C1D",
-            "descriptionHtml": "<p>Dimensions: ? x ? x ?</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>A beautiful light sculpture created using a handcrafted algorithm. Each edition of this sculpture is its own unique permutation of the algorithm. The base is handmade ceramic and encases an energy efficient, USB powered LED light. The shade is 3D printed in recycled plastic and when pressed gently from above, turns the light on and off with a satisfying click.</p>\n<p><b>Materials:</b> Recycled plastic, ceramic, LED light</p>\n<p><b>Light source:</b> Built-in 1w LED light. Fits a standard USB-A socket (wall adapter not included)</p>\n<p><b>Light colour:</b> Warm white (2700 Kelvin)</p>\n<p><b>Dimensions:</b> 50mm x 50mm x 130mm, cable length 2m</p>\n<p>Made in London, UK.</p>\n<p>We care about our impact on the planet, so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": false,
             "priceRange": {
               "maxVariantPrice": {
@@ -127,7 +127,7 @@ const data = {
             "id": "gid://shopify/Product/7295547900099",
             "handle": "ripple-light-sculpture-edition-a715",
             "title": "Ripple Light Sculpture - Edition A715",
-            "descriptionHtml": "<p>Dimensions: ? x ? x ?</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>A beautiful light sculpture created using a handcrafted ripple algorithm. Each edition of this sculpture is its own unique permutation of the algorithm. The base is handmade ceramic and encases an energy efficient, USB powered LED light. The shade is 3D printed in recycled plastic and when pressed gently from above, turns the light on and off with a satisfying click.</p>\n<p><b>Materials:</b> Recycled plastic, ceramic, LED light</p>\n<p><b>Light source:</b> Built-in 1w LED light. Fits a standard USB-A socket (wall adapter not included)</p>\n<p><b>Light colour:</b> Warm white (2700 Kelvin)</p>\n<p><b>Dimensions:</b> 50mm x 50mm x 120mm, cable length 2m</p>\n<p>Made in London, UK.</p>\n<p>We care about our impact on the planet, so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": false,
             "priceRange": {
               "maxVariantPrice": {
@@ -156,7 +156,7 @@ const data = {
             "id": "gid://shopify/Product/7295548227779",
             "handle": "ripple-light-sculpture-edition-a734",
             "title": "Ripple Light Sculpture - Edition A734",
-            "descriptionHtml": "<p>Dimensions: ? x ? x ?</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>A beautiful light sculpture created using a handcrafted ripple algorithm. Each edition of this sculpture is its own unique permutation of the algorithm. The base is handmade ceramic and encases an energy efficient, USB powered LED light. The shade is 3D printed in recycled plastic and when pressed gently from above, turns the light on and off with a satisfying click.</p>\n<p><b>Materials:</b> Recycled plastic, ceramic, LED light</p>\n<p><b>Light source:</b> Built-in 1w LED light. Fits a standard USB-A socket (wall adapter not included)</p>\n<p><b>Light colour:</b> Warm white (2700 Kelvin)</p>\n<p><b>Dimensions:</b> 50mm x 50mm x 120mm, cable length 2m</p>\n<p>Made in London, UK.</p>\n<p>We care about our impact on the planet, so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": true,
             "priceRange": {
               "maxVariantPrice": {
@@ -185,7 +185,7 @@ const data = {
             "id": "gid://shopify/Product/7260086239427",
             "handle": "finn-lamp-shade",
             "title": "Finn Lamp Shade",
-            "descriptionHtml": "<p>Finn is a 3D printed lamp shade made completely from recycled bioplastic. The design is based on a cubic oscillation with a subtle distortion through the form.</p>\n<p>Finn is made from 98% recycled bioplastic (the remaining 2% is the white tint). The material used was originally produced from corn starch then recycled again from food packaging giving a second life in a much longer living product.</p>\n<p>Finn fits a standard threaded E27 bulb holder (40mm diameter). For example it will fit the IKEA Skaftet E27 floor lamp base or the Sunneby E27 pendant cord. You can also use Finn with smaller fittings like an E14 or B22. You just need to combine the fitting with a reducer ring.</p>\n<p>Dimensions: W165mm x D165mm x H165mm</p>\n<p>Please use energy-saving light bulbs or LED light bulbs only with a maximum of 11 watts.</p>\n<p>Manufactured in London, UK.</p>\n<p>We care about our impact on the planet so we ship Finn with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>Finn is a 3D printed lamp shade made completely from recycled bioplastic. The design is based on a cubic oscillation with a subtle distortion through the form.</p>\n\n<p>Finn is made from 98% recycled bioplastic (the remaining 2% is the white tint). The material used was originally produced from corn starch then recycled again from food packaging giving a second life in a much longer living product.</p>\n\n<p>Finn fits a standard threaded E27 bulb holder (40mm diameter). For example it will fit the IKEA Skaftet E27 floor lamp base or the Sunneby E27 pendant cord. You can also use Finn with smaller fittings like an E14 or B22. You just need to combine the fitting with a reducer ring.</p>\n\n<p><b>Dimensions:</b> W165mm x D165mm x H165mm</p>\n\n<p>Please use energy-saving light bulbs or LED light bulbs only with a maximum of 11 watts.</p>\n\n<p>Manufactured in London, UK.</p>\n\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": true,
             "priceRange": {
               "maxVariantPrice": {
@@ -234,7 +234,7 @@ const data = {
             "id": "gid://shopify/Product/7260085158083",
             "handle": "ripple-lamp-shade",
             "title": "Ripple Lamp Shade",
-            "descriptionHtml": "<p>Ripple is a 3D printed lamp shade made completely from recycled bioplastic. The form is based on an oscillating wave with a subtle distortion.</p>\n\n<p>Ripple is made from 98% recycled bioplastic (the remaining 2% is the white tint). The material used was originally produced from corn starch then recycled again from food packaging giving a second life in a much longer living product.</p>\n\n<p>Ripple can be used either as a table lamp shade or a pendant lamp shade. Ripple fits a standard threaded E14 or B22 bulb holder (28mm diameter), e.g. it will fit the IKEA Skaftet E14 table lamp base or the Strala E14 pendant cord.</p>\n\n<p>Dimensions: W130mm x D130mm x H150mm</p>\n\n<p>Please use energy-saving light bulbs or LED light bulbs only with a maximum of 11 watts.</p>\n\n<p>Manufactured in London, UK.</p>\n\n<p>We care about our impact on the planet so we ship Ripple with 100% recyclable packaging and labels.</p>",
+            "descriptionHtml": "<p>Ripple is a 3D printed lamp shade made completely from recycled bioplastic. The form is based on an oscillating wave with a subtle distortion.</p>\n\n<p>Ripple is made from 98% recycled bioplastic (the remaining 2% is the white tint). The material used was originally produced from corn starch then recycled again from food packaging giving a second life in a much longer living product.</p>\n\n<p>Ripple can be used either as a table lamp shade or a pendant lamp shade. Ripple fits a standard threaded E14 or B22 bulb holder (28mm diameter), e.g. it will fit the IKEA Skaftet E14 table lamp base or the Strala E14 pendant cord.</p>\n\n<p><b>Dimensions:</b> W130mm x D130mm x H150mm</p>\n\n<p>Please use energy-saving light bulbs or LED light bulbs only with a maximum of 11 watts.</p>\n\n<p>Manufactured in London, UK.</p>\n\n<p>We care about our impact on the planet so we ship our products with 100% recyclable packaging and labels.</p>",
             "availableForSale": true,
             "priceRange": {
               "maxVariantPrice": {
